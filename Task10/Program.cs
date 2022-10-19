f9a44ab1ff07879bd012eecc153bf35670c5c851
Alex Kizilov <as_k@list.ru>
2022-10-19 10:28:52 +0400

    Task10 copleted

---
+++ Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿/*  Программа, которая принимает на вход 
+ *  трёхзначное число и на выходе показывает 
+ *  вторую цифру этого числа.
+ */
+
+Console.Clear();
+Console.WriteLine("Введите любое трехзначное число: ");
+Console.WriteLine();
+
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+// Используем Abs на случай, если пользователь введет отрицательное число.  
+
+a = Math.Abs(a);
+
+if (a < 99 || a > 999) 
+    {
+    Console.WriteLine("Вы ввели не трехзначное число, попробуйте еще раз");
+    Console.WriteLine();
+    }
+else
+    {
+    a = a % 100 / 10;
+    Console.WriteLine($"Вторая цифра введенного числa - это  " + a);
+    Console.WriteLine();
+    }    
